d2347ffcb18531ee968489c17d383737af16a663
Binkabreezy <binaminbinka@gmail.com>
2023-07-13 09:52:28 -0700

    Puzzle c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * *main - print a given phrase 
+ * Return: 0 if exited properly, non zero otherwise
+ */
+
+int main(void)
+ 
+{
+	put("\programming is like building a multilingual puzzle"
+			return (0);
+			
+
+			}
