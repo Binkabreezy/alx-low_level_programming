8c23d29e9e921965400ae1231fb5efc2125a480b
Binkabreezy <binaminbinka@gmail.com>
2023-07-24 02:36:07 -0700

    swap

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,11 @@
-#ifndef MAIN.H
-#define MAIN.H
-int _putchar(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+int _putchar(char);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
